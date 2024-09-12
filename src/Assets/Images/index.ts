4964910689c581c51b60dc8482accc5b334061e7
mugheera-bilal <mugheera.bilal@metafroliclabs.com>
2024-09-12 00:37:04 -0700

    added images

--- src/Assets/Images/index.ts
+++ src/Assets/Images/index.ts
@@ -1,13 +1,26 @@
-
 export const images = {
-    mainLogo : require('../../../assets/images/mainLogo.png'),
-    mainGreenLogo : require('../../../assets/images/spotifyLogo.png'),
-    mobileLogo : require('../../../assets/images/mobileLogo.png'),
-    googleLogo : require('../../../assets/images/googleLogo.png'),
-    fbLogo : require('../../../assets/images/facebookLogo.png'),
-    searchLogo : require('../../../assets/images/searchLogo.png'),
-    playLogo : require('../../../assets/images/playLogo.png'),
-    perfectCover : require('../../../assets/images/perfect.jpg'),
-    CardPics : require('../../../assets/images/eddd.png')
-    
-}
+  mainLogo: require('../../../assets/images/mainLogo.png'),
+  mainGreenLogo: require('../../../assets/images/spotifyLogo.png'),
+  mobileLogo: require('../../../assets/images/mobileLogo.png'),
+  googleLogo: require('../../../assets/images/googleLogo.png'),
+  fbLogo: require('../../../assets/images/facebookLogo.png'),
+  searchLogo: require('../../../assets/images/searchLogo.png'),
+  playGreenLogo: require('../../../assets/images/playGreenLogo.png'),
+  propertiesLogo: require('../../../assets/images/Properties.png'),
+  perfectCover: require('../../../assets/images/perfect.jpg'),
+
+  playLine: require('../../../assets/images/playLine.png'),
+  bellLogo: require('../../../assets/images/bellLogo.png'),
+  historyLogo: require('../../../assets/images/historyLogo.png'),
+  settingLogo: require('../../../assets/images/settingLogo.png'),
+  arrowDownLogo: require('../../../assets/images/arrowDownLogo.png'),
+  heartLogo: require('../../../assets/images/heartLogo.png'),
+  shufleLogo: require('../../../assets/images/shufleLogo.png'),
+  previousPlayLogo: require('../../../assets/images/previousPlayLogo.png'),
+  pauseLogo: require('../../../assets/images/pauseLogo.png'),
+  nextPlayLogo: require('../../../assets/images/nextPlayLogo.png'),
+  loopLogo: require('../../../assets/images/loopLogo.png'),
+  castLogo: require('../../../assets/images/castLogo.png'),
+  shareLogo: require('../../../assets/images/shareLogo.png'),
+  CardPics: require('../../../assets/images/eddd.png'),
+};
